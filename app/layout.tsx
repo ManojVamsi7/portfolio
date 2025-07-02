cf9f4ec5df68864bdd8fb5bf8a0604611760514f
Manoj Vamsi <143374914+ManojVamsi7@users.noreply.github.com>
2025-07-02 11:30:33 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Vamsi | Portfolio",
+  title: "Manoj Vamsi Dakamarri",
   description: "Personal portfolio of Manoj Vamsi, a computer science graduate and full-stack developer",
     generator: 'v0.dev'
 }
